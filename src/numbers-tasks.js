6313321faf375de75109fc91e65723fee6fdd910
Elena Mankerova <esrudenko93@gmail.com>
2023-11-07 01:31:03 +0600

    feat: done 6-10 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,8 +108,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const XY = x1 * x2 + y1 * y2;
+  const X = (x1 ** 2 + y1 ** 2) ** (1 / 2);
+  const Y = (x2 ** 2 + y2 ** 2) ** (1 / 2);
+  const cosA = XY / X / Y;
+  return Math.acos(cosA);
 }
 
 /**
@@ -125,8 +129,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -140,8 +144,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -157,8 +161,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return (a ** 2 + b ** 2 + c ** 2) ** (1 / 2);
 }
 
 /**
@@ -178,8 +182,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const numDec = Number(`1${'0'.repeat(pow * 2)}`);
+  return (num / numDec).toFixed(pow) * numDec;
 }
 
 /**
